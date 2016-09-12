1e9d64ca8a2228f3f26d49b91f4b29c5d1752956
Administrator <Administrator@128.96.1.39>
2016-09-12 00:35:55 +0000

    screenshot class

--- src/application/main/stepDefImpl/LoginPage.java
+++ src/application/main/stepDefImpl/LoginPage.java
@@ -1,49 +1,47 @@
 package application.main.stepDefImpl;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class LoginPage {
 
-	public static void main(String args[]) throws InterruptedException
+	public static void main(String args[])
 	{
-		List<String> features= new ArrayList<String>();
-		System.setProperty("webdriver.firefox.marionette","C:\\Users\\Administrator\\Downloads\\geckodriver-v0.9.0-win64\\geckodriver.exe");
-		WebDriver driver= new FirefoxDriver();
-		driver.get("http://www.amazon.com");
-		driver.manage().window().maximize();
-		Thread.sleep(5000);
-		driver.findElement(By.xpath(".//*[@id='twotabsearchtextbox']")).sendKeys("latest deals");
-		driver.findElement(By.xpath(".//*[@id='nav-search']/form/div[2]/div/input")).click();
-		List<WebElement> ele= driver.findElements(By.xpath(".//li//h2[contains(text(),'Samsung')]"));
-		java.util.Iterator<WebElement> a= ele.iterator();
-		while(a.hasNext())
-		{
-			WebElement ele1=a.next();
-			System.out.println("Product Name: "+ele1.getText());
-			ele1.click();
-		}
-		Thread.sleep(5000);
-		System.out.println("price: "+driver.findElement(By.xpath(".//*[@id='priceblock_ourprice']")).getText());
-		List<WebElement> ele2= driver.findElements(By.xpath(".//*[@id='feature-bullets']/ul/li/span"));
-		java.util.Iterator<WebElement> b= ele2.iterator();
-		while(b.hasNext())
-		{
-			WebElement ele3=b.next();
-			features.add(ele3.getText());
-		}
-		System.out.println("------------------Features----------------------");
-		for(String c: features)
-		{
-		System.out.println("* "+c);
-		}
-		driver.quit();
+		LoginPage login= new LoginPage();
+		login.loginValidation("mvenkat@dhs.gov","inaflash");
 	}
 
+	@SuppressWarnings("deprecation")
+	public void loginValidation(String username, String password)
+	{
+		System.setProperty("webdriver.firefox.marionette","C:\\Users\\Administrator\\Downloads\\geckodriver-v0.9.0-win64\\geckodriver.exe");
+		WebDriver driver= new FirefoxDriver();
+		driver.get("http://52.90.148.37:8080/flash-ui");
+		driver.manage().window().maximize();
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		driver.findElement(By.xpath(".//input[@placeholder='Username']")).sendKeys(username);
+		driver.findElement(By.xpath(".//input[@placeholder='Password']")).sendKeys(password);
+		driver.findElement(By.xpath("//button[@name='submit']")).click();
+		
+		if(driver.findElement(By.xpath(".//span[@class='glyphicon glyphicon-exclamation-sign']")).isDisplayed()==true)
+		{
+			
+		}
+		
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Assert.assertEquals("DHS FLASH", driver.findElement(By.xpath(".//*[@id='content-container']/h1")).getText().trim());
+		driver.quit();
+	}
 }
---
+++ src/application/main/stepDefImpl/Screenshot.java
@@ -0,0 +1,42 @@
+package application.main.stepDefImpl;
+
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+public class Screenshot {
+
+	public void takeScreenshot(String className,String message)
+	{
+		try{
+		boolean success=false;
+		
+		String dir="C:\\Users\\Administrator\\Desktop\\DhsProject\\screenshots"+className;
+		
+		File screenStoreDir= new File(dir);
+		
+		if(screenStoreDir.exists()){
+			
+		}else{
+			success=screenStoreDir.mkdir();
+			if(success){
+				
+			}else
+			{
+			System.out.printf("failed to create new directory");
+			}
+		}
+		
+		BufferedImage screenShot = new Robot().createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+        ImageIO.write(screenShot, "JPG", new File(screenStoreDir+"//"+message+".jpg"));
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+}
