88b1e1d8ea3d4fe7f9a0222a7f60fbdafb2135bb
Administrator <Administrator@128.96.1.39>
2016-09-12 17:07:48 +0000

    final commit

--- src/application/main/stepDefImpl/Main.java
+++ src/application/main/stepDefImpl/Main.java
@@ -11,16 +11,16 @@ public class Main {
 		login.loginValidation("123","inaflash");
 		login.loginValidation("****", "((((((");
 		
-		login.loginUI("praj@dhs.gov","inaflash");
-		employee.createEmployee("demo", "nizampatnam", "demouser4@dhs.gov", "inaflash", "IT");
-		login.loginValidation("demouser4@dhs.gov", "inaflash");
-		login.loginUI("praj@dhs.gov","inaflash");
-		employee.createEmployee("demo", "nizampatnam", "demouser4@dhs.gov", "inaflash", "IT");
+		login.loginUI("amurphy@dhs.gov","inaflash");
+		employee.createEmployee("demo2", "demo2", "demouser6@dhs.gov", "inaflash", "IT");
+		login.loginValidation("demouser6@dhs.gov", "inaflash");
+		login.loginUI("amurphy@dhs.gov","inaflash");
+		employee.createEmployee("demo2", "demo2", "demouser6@dhs.gov", "inaflash", "IT");
 	
 		GiveKudos kudos= new GiveKudos();
-		login.loginUI("demouser4@dhs.gov","inaflash");
-		kudos.giveKudosToEmpl0yee("a", "Teamwork","Good Work");
-		login.loginUI("demouser4@dhs.gov","inaflash");
-		kudos.giveKudosToEmpl0yee("a", "Teamwork","Good Work");
+		login.loginUI("demouser6@dhs.gov","inaflash");
+		kudos.giveKudosToEmpl0yee("1", "Teamwork","Good Work");
+		login.loginUI("demouser6@dhs.gov","inaflash");
+		kudos.giveKudosToEmpl0yee("1", "Teamwork","Good Work");
    }
 }
